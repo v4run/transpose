0d770bb62477347ac2f6e3d383d593cbc1ee4c66
Varun Vasan V <varunvasan91@gmail.com>
2016-02-13 14:40:49 +0530

    code refactor and packaging fix

--- src/transpose.ts
+++ src/transpose.ts
@@ -1,10 +1,10 @@
 'use strict';
 
-import * as vscode from 'vscode';
+import {commands, ExtensionContext, Position, Selection, TextEditor, TextEditorEdit, window} from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerTextEditorCommand('extension.transpose', (textEditor: vscode.TextEditor,
-        edit: vscode.TextEditorEdit) => {
+export function activate(context: ExtensionContext) {
+    let disposable = commands.registerTextEditorCommand('extension.transpose', (textEditor: TextEditor,
+        edit: TextEditorEdit) => {
         let transposer = new Transposer(textEditor, edit);
         transposer.transpose()
     })
@@ -13,22 +13,21 @@ export function activate(context: vscode.ExtensionContext) {
 
 // Transposer class
 class Transposer {
-    private _textEditor: vscode.TextEditor;
-    private _textEditorEdit: vscode.TextEditorEdit;
+    private _textEditor: TextEditor;
+    private _textEditorEdit: TextEditorEdit;
 
-    constructor(editor: vscode.TextEditor, editorEdit: vscode.TextEditorEdit) {
+    constructor(editor: TextEditor, editorEdit: TextEditorEdit) {
         this._textEditorEdit = editorEdit;
         this._textEditor = editor;
     }
 
-    private _isEmpty(s: vscode.Selection, i: number, a: vscode.Selection[]) {
+    private _isEmpty(s: Selection, i: number, a: Selection[]) {
         return s.isEmpty
     }
 
     // Finds next character position of the given position
     // Returns null if final character
-    private _nextChar(position: vscode.Position) {
-        let s = new vscode.Selection(position, position)
+    private _nextChar(position: Position) {
         var nextLine;
         var nextCharacter;
         let lastPositionInLine = this._textEditor.document.lineAt(position.line).range.end;
@@ -42,13 +41,12 @@ class Transposer {
             nextLine = position.line;
             nextCharacter = position.character + 1;
         }
-        return new vscode.Position(nextLine, nextCharacter)
+        return new Position(nextLine, nextCharacter)
     }
 
     // Finds previous character position of the given position
     // Returns null if first character
-    private _prevChar(position: vscode.Position) {
-        let s = new vscode.Selection(position, position)
+    private _prevChar(position: Position) {
         var prevLine;
         var prevCharacter;
         if ((position.line == 0) && (position.character == 0)) {
@@ -61,19 +59,19 @@ class Transposer {
             prevLine = position.line;
             prevCharacter = position.character - 1;
         }
-        return new vscode.Position(prevLine, prevCharacter);
+        return new Position(prevLine, prevCharacter);
     }
 
     // Transpose characters when no selections are made
-    public transposeCharacters(selections: vscode.Selection[]) {
+    public transposeCharacters(selections: Selection[]) {
         selections.forEach(selection => {
-            let p = new vscode.Position(selection.active.line, selection.active.character)
+            let p = new Position(selection.active.line, selection.active.character)
             let nextPosition = this._nextChar(p);
             let prevPosition = this._prevChar(p);
             if (nextPosition == null || prevPosition == null) {
                 return
             }
-            let nextSelection = new vscode.Selection(p, this._nextChar(p))
+            let nextSelection = new Selection(p, this._nextChar(p))
             let nextChar = this._textEditor.document.getText(nextSelection);
             this._textEditorEdit.delete(nextSelection)
             this._textEditorEdit.insert(prevPosition, nextChar)
@@ -100,7 +98,7 @@ class Transposer {
     }
 
     // replace selection s2 with s1
-    private _replaceSelections(s1: vscode.Selection, s2: vscode.Selection) {
+    private _replaceSelections(s1: Selection, s2: Selection) {
         let range;
         let prevSelection;
         if (s1.isEmpty) {
@@ -117,7 +115,7 @@ class Transposer {
     }
 
     // Tranposes the selections
-    public transposeSelection(selections: vscode.Selection[]) {
+    public transposeSelection(selections: Selection[]) {
         let firstSelection = selections[0];
         for (let i = 1; i < selections.length; i++) {
             this._replaceSelections(selections[i], selections[i - 1]);
